957cc2fd1d92b41be73dd974f24909987195d6c4
Priyanshu Gupta <priyanshug1999@gmail.com>
2022-08-17 22:35:47 +0530

    add score modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,16 @@
+function Modal() {
+    return (
+        <>
+            <div id="score" className="modal">
+                <div className="modal-content">
+                    <h4>Your Score</h4>
+                    <p>5</p>
+                </div>
+                <div className="modal-footer">
+                    <a className="modal-close waves-effect waves-green btn-flat">Close</a>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Modal;
