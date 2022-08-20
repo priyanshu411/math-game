99dd018a7f917a81ff19d65380cf93335d072195
Priyanshu Gupta <priyanshug1999@gmail.com>
2022-08-20 19:30:14 +0530

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,14 @@
-function Modal() {
+import { Link } from "react-router-dom";
+function Modal(props) {
     return (
         <>
             <div id="score" className="modal">
                 <div className="modal-content">
                     <h4>Your Score</h4>
-                    <p>5</p>
+                    <p>{props.currScore}</p>
                 </div>
                 <div className="modal-footer">
-                    <a className="modal-close waves-effect waves-green btn-flat">Close</a>
+                    <Link to="/" className="modal-close waves-effect waves-green btn-flat">Close</Link>
                 </div>
             </div>
         </>
